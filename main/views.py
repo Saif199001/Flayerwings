be37b69931c6fe0be9598caa17af9caf56217825
Saif199001 <zeeshasaif8@gmail.com>
2025-05-28 13:04:22 +0530

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request,"home.html")
+def about(request):
+    return HttpResponse("<h1>About Flayer Wings</h1>")
+def contact(request):
+    return HttpResponse("<h1>Contact Flayer Wings</h1>")
